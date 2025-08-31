f956e410662a6933d04583d65887a1d16b3dbbbf
Michelle González <michellegonzalez@MacBook-Air-de-Michelle.local>
2025-08-31 16:10:34 -0600

    Se obtiene el nombre del usuario y se agrega al saludo

--- practicas/3/GonzalezMichelle/holaMundo.c
+++ practicas/3/GonzalezMichelle/holaMundo.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola, mundo\n");
+    char nombre[50];
+    
+    printf("Ingresa tu nombre: ");
+    fgets(nombre, sizeof(nombre), stdin);
+    
+    printf("Hola, %s\n", nombre);
+    
     return 0;
 }
+
