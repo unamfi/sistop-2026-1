c4ccae93847ada157163fd69294c7d5f6b3fc0b8
Emmanuel Lara <emm05lara@gmail.com>
2025-09-01 20:53:21 -0600

    Agrego el archivo version 2 de saludo en python de Emmanuel Lara

--- practicas/3/LaraEmmanuel/saludo.py
+++ practicas/3/LaraEmmanuel/saludo.py
@@ -1,1 +1,11 @@
-print("¡Hola mundo!")
+import sys
+
+def main():
+ if len(sys.argv) > 1:
+    nombre = sys.argv[1]
+    print(f"¡Hola {nombre}!")
+ else:
+    print("¡Hola mundo!")
+
+if __name__ == "__main__":
+    main()
