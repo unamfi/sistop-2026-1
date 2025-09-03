64530c35441145a65a8562b730c24be8583a43a9
Luis Angel Echevarria Aguilar <luis.ech3va@gmail.com>
2025-09-03 04:48:18 -0600

    Versión modificada: Saludo personalizado desde línea de comando

--- practicas/3/EchevarriaLuis/practica3.c
+++ practicas/3/EchevarriaLuis/practica3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hola mundo\n");
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+	printf("Hola, %s!\n", argv[1]);
+    } else {
+	printf("Hola mundo\n");
+    }
     return 0;
 }
