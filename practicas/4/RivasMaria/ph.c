6e691b2d506ba59de5f7448eb9d2a808380449e9
Maria Lucia Rivas <ma.luciarg11@gmail.com>
2025-09-18 19:22:09 -0600

    Agregar .gitignore

--- practicas/4/RivasMaria/ph.c
+++ practicas/4/RivasMaria/ph.c
@@ -5,7 +5,7 @@ int main() {
     int pid = fork();  // Crea un proceso hijo
 
     if (pid == 0) {
-        printf("Mataste a mi padre\n");
+        printf("Tu mataste a mi padre\n");
     } else {
         printf("YO SOY TU PADREEE\n");
     }
