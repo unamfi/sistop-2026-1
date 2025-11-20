2a8edaf3845378698866d831847c0125494feade
YukiGab <yukio.gabriel.h@gmail.com>
2025-11-20 16:09:57 -0600

    Actualizo main y añado comentarios (como en Interfaz Grafica)

--- proyectos/2/GabrielYukioayax/Proyecto 2/Main.java
+++ proyectos/2/GabrielYukioayax/Proyecto 2/Main.java
@@ -1,57 +1,17 @@
-import java.util.Scanner;
-import java.io.File;
+import javax.swing.SwingUtilities;
 public class Main{
-    public static void main( String[] args ){
-        System.out.println( "--- INICIANDO SISTEMA ---" );
-        GestorDisco g = new GestorDisco( "fiunamfs.img" );
-        g.iniciar();        
-        Scanner teclado = new Scanner( System.in );
-        boolean salir = false;
-        while(!salir){
-            System.out.println( "\n\nSelecciona una opcion:" );
-            System.out.println( "1. Listar archivos" );
-            System.out.println( "2. Copiar archivo sacar" );
-            System.out.println( "3. Copiar archivo meter" );
-            System.out.println( "4. Eliminar" );
-            System.out.println( "5. Salir" );
-            System.out.print( "Opcion: " );
-            int op = teclado.nextInt();
-            teclado.nextLine(); 
-            if( op == 1 ){
-                Thread t = new Thread( new Runnable(){
-                    public void run(){
-                        System.out.println( "Leyendo disco..." );
-                        g.leerContenido();
-                    }
-                });
-                t.start();
-            } 
-            else if( op == 2 ){
-                System.out.println( "Escribe el nombre del archivo:" );
-                String nombre = teclado.nextLine();
-                g.sacarArchivo( nombre ); 
+    public static void main(String[] args){
+        //Iniciar la interfaz grafica
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                try {
+                    //Creamos y mostramos la ventana
+                    InterfazGrafica ventana = new InterfazGrafica();
+                    ventana.setVisible(true);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
             }
-            else if( op == 3 ){    
-                System.out.println( "Dime la ruta de tu compu:" );
-                String ruta = teclado.nextLine();
-                g.meterArchivo(ruta);
-            }
-            else if( op == 4 ){
-                System.out.println("Cual archivo borro?:");
-                String b = teclado.nextLine();
-                g.eliminarArchivo(b);   
-            }
-            else if( op == 5 ){
-                salir = true;
-                g.cerrar();
-                System.out.println( "Adios" );
-            }
-            else {
-                System.out.println( "Opcion no valida" );
-            }
-            try{
-                Thread.sleep( 500 );
-            } catch(Exception e){}
-        }  
+        });
     }
 }
