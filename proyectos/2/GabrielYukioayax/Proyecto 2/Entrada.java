ef16972a17396f6c069b9354dcac387e40e7e088
YukiGab <yukio.gabriel.h@gmail.com>
2025-11-20 07:29:56 -0600

    Pulo codigo y corrijo

--- proyectos/2/GabrielYukioayax/Proyecto 2/Entrada.java
+++ proyectos/2/GabrielYukioayax/Proyecto 2/Entrada.java
@@ -1,11 +1,20 @@
-public class Entrada {
-
+public class Entrada{
     public String nombre;
     public int clusterInicial;
     public int tamanoBytes;
     public String fechaCreacion;
     public String fechaModificacion;
-
+    public Entrada(byte[] datos){
+        byte[] n = new byte[14];
+        for(int i = 0; i < 14; i++) {
+            n[i] = datos[i + 1];
+        }
+        this.nombre = new String(n);
+        this.clusterInicial = (datos[16] & 0xFF) | ((datos[17] & 0xFF) << 8) | ((datos[18] & 0xFF) << 16) | ((datos[19] & 0xFF) << 24);
+        this.tamanoBytes = (datos[20] & 0xFF) | ((datos[21] & 0xFF) << 8) | ((datos[22] & 0xFF) << 16) | ((datos[23] & 0xFF) << 24);   
+        this.fechaCreacion = "";
+        this.fechaModificacion = "";
+    }
     public Entrada(String n, int c, int t, String fc, String fm){
         this.nombre = n;
         this.clusterInicial = c;
@@ -13,14 +22,11 @@ public class Entrada {
         this.fechaCreacion = fc;
         this.fechaModificacion = fm;
     }
-
     public Entrada(){
-
     }
-
     public boolean esValido(){
         try {
-            if(nombre.charAt(0) == '-'){
+            if(nombre.charAt(0) == '-' || nombre.charAt(0) == 45) {
                 return false;
             }
             return true;
@@ -28,17 +34,14 @@ public class Entrada {
             return false;
         }
     }
-
     public String getDatos(){
         return "Archivo: " + nombre + " | Cluster: " + clusterInicial + " | Tam: " + tamanoBytes;
     }
-
     public void limpiarNombre(){
         try {
-            this.nombre = this.nombre.trim();
-        } catch(Exception e){
-            
+            String s = this.nombre.replace((char)0, ' ');
+            this.nombre = s.trim();
+        } catch(Exception e) {
         }
     }
-
 }
