5b8d3f7257618b7ab9e5e76d240e535c77918257
Alejandro Chavez <chavezinst26@gmail.com>
2025-11-20 20:32:26 -0600

    Primera parte: implementacion de lectura y validacion del superbloque

---
+++ proyectos/2/ChavezAlejandro/README.md
@@ -0,0 +1,19 @@
+# Proyecto: (Micro) sistema de archivos FiUnamFS
+**Alumno:** B. Alejandro Chávez López
+
+## Descripción
+Este proyecto implementa un manipulador para el sistema de archivos FiUnamFS.
+El sistema debe permitir listar, copiar y eliminar archivos de una imagen de disco virtual (que se incluyó en la asignación).
+
+## Estructura del Proyecto
+    -> fiunamfs.img: Imagen del disco virtual (sirve como simulación de hardware).
+    -> fiunamfs.py: Script principal que implementa las funciones.
+    -> README.md: Este readme en markdown
+
+## Avance
+### Fase 1: Lectura del Superbloque
+Se implementó la función "leer_superbloque" que:
+1. Abre el archivo ".img" en modo binario.
+2. Lee los primeros 54 bytes.
+3. Decodifica los valores ASCII y Little Endian (como se muestra en la asignación).
+4. Valida que el "Magic Number" sea "FiUnamFS" y la versión 26-2.
---
+++ proyectos/2/ChavezAlejandro/fiunamfs.py
@@ -0,0 +1,54 @@
+import struct
+import os
+import sys
+
+# PRIMERA SECCION DEL PROYECTO
+
+def leer_superbloque(ruta_archivo):
+
+    #Lee el superbloque del sistema de archivos FiUnamFS.
+    #Valida que sea el sistema correcto y muestra sus metadatos.
+    
+    try:
+        with open(ruta_archivo, 'rb') as f:
+            #Para aseguramos de estar al inicio del archivo
+            f.seek(0)
+            
+            #Se leen los primeros 54 bytes 
+            superbloque_data = f.read(54)
+            
+            if not superbloque_data:
+                print("Error: El archivo esta vacio o no se pudo leer")
+                return
+
+            nombre_fs = superbloque_data[0:8].decode('ascii').strip()
+            version_fs = superbloque_data[10:14].decode('ascii').strip()
+            etiqueta_vol = superbloque_data[20:36].decode('ascii').strip()
+            
+            # struct.unpack con <I para los enteros
+            tam_cluster = struct.unpack('<I', superbloque_data[40:44])[0]
+            num_cluster_dir = struct.unpack('<I', superbloque_data[45:49])[0]
+            num_cluster_total = struct.unpack('<I', superbloque_data[50:54])[0]
+
+            print(f" SISTEMA DE ARCHIVOS DETECTADO")
+            print(f"Nombre: {nombre_fs}")
+            print(f"Versión: {version_fs}")
+            print(f"Etiqueta: {etiqueta_vol}")
+            print(f"Tamanioo de Cluster: {tam_cluster} bytes")
+            print(f"Clusters de Directorio: {num_cluster_dir}")
+            print(f"Clusters Totales: {num_cluster_total}")
+            
+            #Validacion
+            if nombre_fs == "FiUnamFS" and version_fs == "26-2":
+                print("\n[Correcto] El sistema de archivos es VALIDO")
+            else:
+                print("\n[Error] Sistema de archivos no reconocido (o versión incorrecta)")
+
+    except FileNotFoundError:
+        print(f"Error: No se encuentra el archivo '{ruta_archivo}'")
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+
+if __name__ == "__main__":
+    #Se ejecuta la funcion principal
+    leer_superbloque("fiunamfs.img")
